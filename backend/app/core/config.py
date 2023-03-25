9565aa993e678c149b57996e67800153d959073a
KozielGPC <gpcgabriel0@gmail.com>
2023-03-25 12:54:53 -0300

    #1- add: env connection to db

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,7 +1,18 @@
 import os
+from pydantic import BaseSettings
 
 PROJECT_NAME = "championship-platform"
 
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
 API_V1_STR = "/api/v1"
+
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+# specify .env file location as Config attribute
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-from app.core import config
+from app.core.config import settings
 
 engine = create_engine(
-    config.SQLALCHEMY_DATABASE_URI,
+    settings.DATABASE_URL,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
