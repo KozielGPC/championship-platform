88d66a17ed5439ee3003dcb1f62e8ddcc9bc4787
sircaio21 <caio21122001@gmail.com>
2023-04-03 18:43:08 -0300

    Initial Setup Frontend

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,10 @@
+
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+      <h1>CHAMPIONSHIP-PLATAFORM</h1>
+  )
+}
