b7e181bf85030efd7bcb4124f208f23cc17358f5
KozielGPC <gpcgabriel0@gmail.com>
2023-04-05 22:53:59 -0300

    #46 - iniciado controle de rotas

---
+++ backend/api/controllers/routes.py
@@ -0,0 +1,3 @@
+from .users import router as users_router
+
+routes = users_router
--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI, APIRouter
 
 from api.models import users
 from api.database.config import engine
+from api.controllers import route_users
+
 
 users.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.include_router(route_users.router)
 
 
 @app.get("/")
