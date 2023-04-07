70b25bfabb20f973e78bcc5cef35a8591f2d8d66
KozielGPC <gpcgabriel0@gmail.com>
2023-04-07 18:00:38 -0300

    #46 - adicionado migracoes

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,5 @@
 from fastapi import FastAPI, APIRouter
-
-from api.models import users
-from api.database.config import engine
 from api.routers import routes
-
-
-users.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 for route in routes.routes:
---
+++ backend/migrations/versions/b885273e8a0d_add_users_table.py
@@ -0,0 +1,31 @@
+"""add users table
+
+Revision ID: b885273e8a0d
+Revises: 
+Create Date: 2023-04-07 17:41:48.901729
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "b885273e8a0d"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column("username", sa.String(50), unique=True, nullable=False),
+        sa.Column("password", sa.String(50), unique=True, nullable=False),
+    )
+    pass
+
+
+def downgrade() -> None:
+    op.drop_table("users")
+    pass
