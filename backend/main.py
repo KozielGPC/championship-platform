788760c8179d1af3a380dd1c772125c07f0cc6a1
Marcio Gabriel <gpcgabriel0@gmail.com>
2023-04-24 11:58:15 -0300

    update port

--- backend/main.py
+++ backend/main.py
@@ -21,5 +21,5 @@ for route in routes.routes:
     app.include_router(route)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
