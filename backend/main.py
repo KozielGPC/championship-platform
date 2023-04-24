48fb9cfe9c0f7709e2f2a4d8dc78db1321cf0c0e
Marcio Gabriel <gpcgabriel0@gmail.com>
2023-04-24 11:50:11 -0300

    update okteto stack

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from api.routers import routes
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -15,3 +19,7 @@ app.add_middleware(
 
 for route in routes.routes:
     app.include_router(route)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
