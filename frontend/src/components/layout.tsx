4c02819ca317cf9c79d48463eae024344ca18e90
Rafael <dalacquar@gmail.com>
2023-04-25 13:48:56 -0300

    #39 - Layout melhorado, ainda faltam algumas mudanças

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex, Spacer, Button, Text, StackDivider, VStack} from '@chakra-ui/react';
+import { Box, Flex, Spacer, Button, Text, StackDivider, VStack, Grid, GridItem} from '@chakra-ui/react';
 import {SignoutButton} from './signoutButton'
 import {UserContext} from '../context/UserContext'
 import { useContext } from 'react';
@@ -12,33 +12,64 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
  
 
   return (
-    <Flex height="100vh">
-      <Box bg="red.500" w="25%">
-        <Flex align="center" justify="center" h="10%" borderBottom="1px solid #E2E8F0">
-          <Text fontSize="lg" fontWeight="bold">
-            Championship Plataform
-          </Text>
-        </Flex>
-        <Flex direction="column" h="90%" justify="space-between">
-          <Box>
-            {/* aqui ficaria o menu */}
-          </Box>
-          <Box p="4">
-            <SignoutButton/>
-          </Box>
-        </Flex>
-      </Box>
-      <Box bg="white" w="75%">
-        <Box bg="yellow.500" h="90%">
-          {children} {/* conteudo principal ficaria aqui*/}
-        </Box> 
-        <Box bg="green.500" h="10%">
-          <Text fontSize="lg" fontWeight="bold">
-            RODAPÉ
-          </Text>
-        </Box> 
-      </Box>
-    </Flex>
+    <Grid
+      templateAreas={`"nav main"
+                      "nav footer"`}
+
+      gridTemplateRows={'100% 15vh'}
+      gridTemplateColumns={'20% 80%'}
+      gap='0'
+      color='blackAlpha.700'
+      fontWeight='bold'
+    >
+      <GridItem pl='2' bg='gray.700' area={'nav'}
+        borderRightWidth="4px"
+        borderRightColor="white"
+        borderStyle="solid"
+      >
+        <Box position="fixed" top="0" left="0">
+            <Grid
+              templateAreas={`"nav_main"`}
+              gridTemplateRows={'100vh'}
+              gridTemplateColumns={'38vh'}
+              gap='0'
+              color='blackAlpha.700'
+              fontWeight='bold'
+            >
+              <Box position="relative" w="100%" h="100%">
+                <Box color="White" borderBottomWidth="1px" borderColor="white" borderStyle="solid" textAlign="center" pt="10px" pb="10px" w="100%">JuegosHub</Box>
+                <Box position="absolute" left="10px" bottom="10px"><SignoutButton></SignoutButton></Box>
+              </Box>
+            </Grid>
+        </Box>
+
+          
+      </GridItem>
+      <GridItem pl='2' bg='gray.600' area={'main'}>
+        <Box>
+          {children}
+          Main
+        </Box>
+        <Box>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quas eum itaque ea illum vero, voluptatem, iste aliquid sequi ut labore! Molestias recusandae ratione, quisquam similique iure voluptas sint perspiciatis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quas eum itaque ea illum vero, voluptatem, iste aliquid sequi ut labore! Molestias recusandae ratione, quisquam similique iure voluptas sint perspiciatis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quas eum itaque ea illum vero, voluptatem, iste aliquid sequi ut labore! Molestias recusandae ratione, quisquam similique iure voluptas sint perspiciatis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quas eum itaque ea illum vero, voluptatem, iste aliquid sequi ut labore! Molestias recusandae ratione, quisquam similique iure voluptas sint perspiciatis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quas eum itaque ea illum vero, voluptatem, iste aliquid sequi ut labore! Molestias recusandae ratione, quisquam similique iure voluptas sint perspiciatis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quas eum itaque ea illum vero, voluptatem, iste aliquid sequi ut labore! Molestias recusandae ratione, quisquam similique iure voluptas sint perspiciatis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quas eum itaque ea illum vero, voluptatem, iste aliquid sequi ut labore! Molestias recusandae ratione, quisquam similique iure voluptas sint perspiciatis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quas eum itaque ea illum vero, voluptatem, iste aliquid sequi ut labore! Molestias recusandae ratione, quisquam similique iure voluptas sint perspiciatis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quas eum itaque ea illum vero, voluptatem, iste aliquid sequi ut labore! Molestias recusandae ratione, quisquam similique iure voluptas sint perspiciatis?
+        </Box>
+      </GridItem>
+      <GridItem pl='2' bg='green.600' area={'footer'}>
+        <Box>
+          Footer
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quas eum itaque ea illum vero, voluptatem, iste aliquid sequi ut labore! Molestias recusandae ratione, quisquam similique iure voluptas sint perspiciatis?
+          
+        </Box>
+      </GridItem>
+    </Grid>
   );
 };
 
