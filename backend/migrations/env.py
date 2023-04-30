d5fb224adeabf0c17b923851a926c35af7b85a2d
KozielGPC <gpcgabriel0@gmail.com>
2023-04-30 12:25:56 -0300

    adicionado env variables para alembic config

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -4,10 +4,20 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+section = config.config_ini_section
+
+database_url = os.environ.get("DATABASE_URL")
+
+config.set_section_option(section, "DATABASE_URL", os.environ.get("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -64,9 +74,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
