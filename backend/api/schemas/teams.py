bc1abbcb3cdeaffb14561d6cb43f4a5f81e3c07a
KozielGPC <gpcgabriel0@gmail.com>
2023-05-14 16:46:19 -0300

    #106 - adicionado rota de update de team e validacoes extras na criacao

--- backend/api/schemas/teams.py
+++ backend/api/schemas/teams.py
@@ -1,16 +1,18 @@
 from typing import List, Optional, Generic, TypeVar
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 from pydantic.generics import GenericModel
+from datetime import datetime
 
 T = TypeVar("T")
 
- 
+
 class TeamSchema(BaseModel):
     id: Optional[int] = None
     name: Optional[str] = None
     password: Optional[str] = None
     owner_id: Optional[int] = Field(default=None, foreign_key="users.id")
     game_id: Optional[int] = Field(default=None, foreign_key="games.id")
+    created_at: Optional[datetime] = None
 
     class Config:
         orm_mode = True
@@ -21,8 +23,31 @@ class TeamInput(TeamSchema):
     password: str
     game_id: int
 
+    @validator("name", "password")
+    def check_empty_fields(cls, v):
+        assert v != "", "Empty strings are not allowed."
+        return v
+
+    @validator("game_id")
+    def check_positive_numbers(cls, v):
+        assert v >= 0, "Negative numbers are not allowed."
+        return v
+
     class Config:
         orm_mode = True
+
+
+class TeamUpdateRequest(BaseModel):
+    name: Optional[str] = None
+    password: Optional[str] = None
+
+    @validator("*")
+    def check_empty_fields(cls, v):
+        assert v != "", "Empty strings are not allowed."
+        return v
+
+    class Config:
+        extra = "forbid"
 
 
 class Response(GenericModel, Generic[T]):
