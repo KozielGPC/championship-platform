460c1dcc44ac2ea6d3c9da6b1e82f0d5f2cbeb26
Rafael <dalacquar@gmail.com>
2023-05-15 21:57:55 -0300

    #56 - Dando inicio a page de dados pessoais

--- frontend/src/pages/profile/index.tsx
+++ frontend/src/pages/profile/index.tsx
@@ -2,16 +2,30 @@
 import { parseCookies } from 'nookies'
 import { GetServerSideProps } from 'next'
 import Layout from '../../components/layout'
-import { Box } from '@chakra-ui/react';
+import { Avatar, Box, Button } from '@chakra-ui/react';
 import { useContext } from 'react';
 import {UserContext} from '../../context/UserContext'
+import Link from 'next/link';
 
 function Profile() {
   const {id, username} = useContext(UserContext);
+  const handleEdit = () => {
+    // Lógica para a ação de edição
+  };
+
+
   return (
     <Layout>
-      <Box textColor="white">
-        <h1>{username}</h1>
+      <Box textColor="white" textAlign="center">
+        <Box pt={'40px'} pb={'40px'}>
+          Dados pessoais
+        </Box>
+        <Box>
+          <Avatar backgroundColor={'green.400'} name={`${username}`} />
+          <Box pt="50px">{username}</Box>
+
+          <Link href={`/profile/edit/${id}`}>Editar</Link>
+        </Box>
       </Box>
     </Layout>
   )
