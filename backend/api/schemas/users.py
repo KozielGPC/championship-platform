271e298a30f36e7c0f7105dfb6c76e011850642c
KozielGPC <gpcgabriel0@gmail.com>
2023-05-20 23:57:37 -0300

    #107 - adicionado rota de update de user

--- backend/api/schemas/users.py
+++ backend/api/schemas/users.py
@@ -1,6 +1,7 @@
 from typing import List, Optional, Generic, TypeVar
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 from pydantic.generics import GenericModel
+import re
 
 T = TypeVar("T")
 
@@ -20,8 +21,49 @@ class UserInput(UserSchema):
     password: str
     email: str
 
+    @validator("*")
+    def check_empty_fields(cls, v):
+        assert v != "", "Empty strings are not allowed."
+        return v
+
+    @validator("*")
+    def check_spaced_fields(cls, v):
+        assert v.strip(), "Empty strings are not allowed."
+        return v
+
+    @validator("email")
+    def validate_email(cls, email):
+        if email is not None:
+            assert re.match(r"^[\w\.-]+@[\w\.-]+\.\w+$", email), "Invalid email format."
+        return email
+
     class Config:
         orm_mode = True
+
+
+class UserUpdateRequest(BaseModel):
+    username: Optional[str] = None
+    password: Optional[str] = None
+    email: Optional[str] = None
+
+    @validator("*")
+    def check_empty_fields(cls, v):
+        assert v != "", "Empty strings are not allowed."
+        return v
+
+    @validator("*")
+    def check_spaced_fields(cls, v):
+        assert v.strip(), "Empty strings are not allowed."
+        return v
+
+    @validator("email")
+    def validate_email(cls, email):
+        if email is not None:
+            assert re.match(r"^[\w\.-]+@[\w\.-]+\.\w+$", email), "Invalid email format."
+        return email
+
+    class Config:
+        extra = "forbid"
 
 
 class Response(GenericModel, Generic[T]):
@@ -29,6 +71,3 @@ class Response(GenericModel, Generic[T]):
     status: str
     message: str
     result: Optional[T]
-
-
-
