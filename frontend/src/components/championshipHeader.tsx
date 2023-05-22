1299fd28ccac5f35136b9b0acd0a38d215853e0c
Joao Mateus <joaomateus@alunos.utfpr.edu.br>
2023-05-22 17:17:19 -0300

    #59 - Melhorando estilização da página

--- frontend/src/components/championshipHeader.tsx
+++ frontend/src/components/championshipHeader.tsx
@@ -30,39 +30,56 @@ const ChampionshipHeader: React.FC<ChampionshipHeaderProps> = ({ championship })
   };
  
   return (
-        <Box>
+        <Box textColor={'white'}>
+          <Image
+            align={'5'}
+            borderTopRadius="10px"
+            width={"100%"}
+            maxHeight={'200px'}
+            objectFit={'contain'}
+            overflow={'hidden'}
+            alt={'lol-image'}
+            src={
+              championship?.game_id == 1 
+              ?'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcStJOZJ8I6GWv1kqOJVgJL7EsLYfLmiL-Vxbu7BpPrurPsUHXvE'
+              : 'https://viendidong.com/wp-content/uploads/2022/12/valorant-thumb-vdd-viendidong.jpg'
+            }
+            />
             <Heading textAlign={'center'} pt={'20px'} h={'10vh'}>
                {championship?.name}
             </Heading>
-            <Tabs colorScheme={'blackAlpha'} size={'lg'} textAlign={'center'} align={'center'} borderColor={'black'}>
+            <Tabs marginTop={'5'} colorScheme={'whiteAlpha'} size={'md'} textAlign={'center'} align={'center'} borderColor={'white'} isFitted={true}>
                 <TabList>
-                    <Tab>Regras</Tab>
-                    <Tab>Premiação</Tab>
-                    <Tab>Equipes</Tab>
-                    <Tab>Contato</Tab>
+                    <Tab>Rules</Tab>
+                    <Tab>Prizes</Tab>
+                    <Tab>Teams</Tab>
+                    <Tab>Contact</Tab>
                 </TabList>
-
             <TabPanels>
                 <TabPanel>{championship?.rules}</TabPanel>
                 <TabPanel>{championship?.prizes}</TabPanel>
-                <TabPanel>{'tet'}</TabPanel>
+                <TabPanel>{'Listagem de time'}</TabPanel>
                 <TabPanel>{championship?.contact}</TabPanel>
             </TabPanels>
             </Tabs>
-            <Heading h={'4vh'} size={'sm'} bottom={'100%'}>Selecione um time:</Heading>
+            <Heading h={'4vh'} size={'sm'} textAlign={'center'} marginTop={'200px'} bottom={'10'}>Select a team:</Heading>
             <Select
+                  size={'sm'}
+                  left={'1'}
+                  width={"99%"}
+                  iconSize={'100px'}
                   name="game_id"
                   value={selectedTeam || ''}
                   onChange={handleTeamChange}
                   >
-                  <option value="">Selecione...</option>
+                  <option value="">Select...</option>
                   {teamsChampionship.map((team) => (
                     <option key={team.id} value = {team.id}>
                       {team.name}
                     </option>
                   ))}
               </Select>
-            <Button colorScheme='blue' position='fixed' left={'57%'} bottom={'35%'} size={'md'}>Join</Button>
+            <Button colorScheme='blue' left={'47%'} size={'md'} top={'3'} marginBottom={'25'}>Join</Button>
         </Box>
   );
 };
