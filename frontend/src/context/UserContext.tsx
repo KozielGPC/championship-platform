4cf60862897e3c8c1490b42513d10dee14d548f9
CAIO SANTOS <devcaiosantos@gmail.com>
2023-06-19 00:00:44 -0300

    3 some message

--- frontend/src/context/UserContext.tsx
+++ frontend/src/context/UserContext.tsx
@@ -44,6 +44,31 @@ const initialValue = {
 }
 
 
+const notificationsFake:Notification[] = [
+    {
+        id: 1,
+        name: "Teste1",
+        reference_user_id: 2,
+        text: "You were invited to the XD Biribiri team",
+        visualized: false
+    },
+    {
+        id: 2,
+        name: "Teste2",
+        reference_user_id: 2,
+        text: "You were invited to the FULLCLEAR team",
+        visualized: true
+    },
+    {
+        id: 3,
+        name: "Teste3",
+        reference_user_id: 3,
+        text: "You were invited to the 777Bros team",
+        visualized: false
+    }
+]
+
+
 export const UserContext = createContext<UserContextType>(initialValue);
 
 
@@ -74,10 +99,10 @@ export const UserProvider = ({ children }: UserContextProps)=>{
 
     async function getNotifications(){
         const response = await getMyNotifications();
-        
         if(response && response.data){
             const notificationsArray :Notification[] = response.data;
-            setNotifications(notificationsArray)
+            //setNotifications(notificationsArray);
+            setNotifications(notificationsFake)
         }
     }
 
