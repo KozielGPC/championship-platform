895aff323c2e055fb3cf6e0840f505a646a21bfe
KozielGPC <gpcgabriel0@gmail.com>
2023-06-26 17:29:57 -0300

    #97.2 - atualizado ordencacao de notificacoes

--- frontend/src/context/UserContext.tsx
+++ frontend/src/context/UserContext.tsx
@@ -1,134 +1,129 @@
-import { createContext, ReactNode,useEffect,useState } from "react";
-import jwt_decode from "jwt-decode"
-import {User} from "../interfaces";
-import {destroyCookie, parseCookies} from 'nookies'
+import { createContext, ReactNode, useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
+import { User } from "../interfaces";
+import { destroyCookie, parseCookies } from "nookies";
 import { useRouter } from "next/router";
 import { getUserById, getMyNotifications } from "@/services/users/retrieve";
 import { Token, Notification } from "../interfaces";
 
 type UserContextProps = {
-    children: ReactNode;
-}
+  children: ReactNode;
+};
 
 type UserContextType = {
-    id: Number;
-    setId: (id: Number) => void;
-    username: string;
-    setUsername: (username: string) => void;
-    email: string;
-    setEmail: (email: string) => void;
-    notifications: Notification[];
-    setNotifications: (notifications: Notification[]) => void;
-    clearUser: () => void;
-    signin: (token:string) => void;
-    signout: () => void;
-    getNotifications: () => void;
-}
-
-
+  id: Number;
+  setId: (id: Number) => void;
+  username: string;
+  setUsername: (username: string) => void;
+  email: string;
+  setEmail: (email: string) => void;
+  notifications: Notification[];
+  setNotifications: (notifications: Notification[]) => void;
+  clearUser: () => void;
+  signin: (token: string) => void;
+  signout: () => void;
+  getNotifications: () => void;
+};
 
 const initialValue = {
-    id: -1,
-    setId: () => {},
-    username: "",
-    setUsername: () => {},
-    email: "",
-    setEmail: () => {},
-    notifications: [],
-    setNotifications: () => {},
-    clearUser: () => {},
-    signin: () => {},
-    signout: () => {},
-    getNotifications: () => {}
-    
-}
-
+  id: -1,
+  setId: () => {},
+  username: "",
+  setUsername: () => {},
+  email: "",
+  setEmail: () => {},
+  notifications: [],
+  setNotifications: () => {},
+  clearUser: () => {},
+  signin: () => {},
+  signout: () => {},
+  getNotifications: () => {},
+};
 
 export const UserContext = createContext<UserContextType>(initialValue);
 
+export const UserProvider = ({ children }: UserContextProps) => {
+  const router = useRouter();
 
-export const UserProvider = ({ children }: UserContextProps)=>{
-    const router = useRouter();
-    
+  const [id, setId] = useState<Number>(initialValue.id);
+  const [username, setUsername] = useState<string>(initialValue.username);
+  const [email, setEmail] = useState<string>(initialValue.email);
+  const [notifications, setNotifications] = useState<Notification[]>([]);
 
-    const [id, setId] = useState<Number>(initialValue.id);
-    const [username, setUsername] = useState<string>(initialValue.username);
-    const [email, setEmail] = useState<string>(initialValue.email);
-    const [notifications, setNotifications] = useState<Notification[]>([]);
+  const token = parseCookies()["championship-token"];
 
-    const token = parseCookies()["championship-token"];
-
-    useEffect(
-        () => {
-            if(token){
-                signin(token)
-                getNotifications();
-            };
-        },[token]
-    )
-
-    function clearUser(){
-        setId(-1);
-        setUsername("");
+  useEffect(() => {
+    if (token) {
+      signin(token);
+      getNotifications();
     }
+  }, [token]);
 
-    async function getNotifications(){
-        const response = await getMyNotifications();
-        if(response && response.data){
-            const notificationsArray :Notification[] = response.data;
-            setNotifications(notificationsArray);
-            //setNotifications(notificationsFake)
-        }
+  function clearUser() {
+    setId(-1);
+    setUsername("");
+  }
+
+  async function getNotifications() {
+    const response = await getMyNotifications();
+    if (response && response.data) {
+      const notificationsArray: Notification[] = response.data;
+      if (notificationsArray) {
+        setNotifications(notificationsArray.reverse());
+      }
     }
+  }
 
+  useEffect(() => {
+    console.log(notifications);
+  }, [notifications]);
 
-    async function signin(token:string){
-        try{
-            const tokenData:Token = jwt_decode(token);
-            if(!tokenData.id){
-                throw new Error("Invalid token");
-            }
-            const response = await getUserById(tokenData.id.toString());
-            if(response.status == "error" || !response.data )throw new Error(response.message);
-           
-            const userData:User = response.data;
+  async function signin(token: string) {
+    try {
+      const tokenData: Token = jwt_decode(token);
+      if (!tokenData.id) {
+        throw new Error("Invalid token");
+      }
+      const response = await getUserById(tokenData.id.toString());
+      if (response.status == "error" || !response.data)
+        throw new Error(response.message);
 
-            setId(userData.id);
-            setUsername(userData.username);
-            setEmail(userData.email)
-        }
-        catch{
-            signout()
-        }
+      const userData: User = response.data;
+
+      setId(userData.id);
+      setUsername(userData.username);
+      setEmail(userData.email);
+    } catch {
+      signout();
     }
+  }
 
-    function signout(){
-        destroyCookie(null,'championship-token',{path: '/'});
-        clearUser();
-        router.push('/signin');
-    }
+  function signout() {
+    destroyCookie(null, "championship-token", { path: "/" });
+    clearUser();
+    router.push("/signin");
+  }
 
-    return (
-        <UserContext.Provider
-            
-            value={{
-                id, setId,
-                username,setUsername,
-                email, setEmail,
-                notifications,setNotifications,
-                clearUser,
-                signin, signout,
-                getNotifications
-            }}
-
-        >
-            {children}
-        </UserContext.Provider>
-    )
-}
-
-
+  return (
+    <UserContext.Provider
+      value={{
+        id,
+        setId,
+        username,
+        setUsername,
+        email,
+        setEmail,
+        notifications,
+        setNotifications,
+        clearUser,
+        signin,
+        signout,
+        getNotifications,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
 
 export default UserContext;
-
-
