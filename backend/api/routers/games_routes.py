f0778036bc4192735e25e6d7f1f7f1d7e91ed559
KozielGPC <gpcgabriel0@gmail.com>
2023-07-02 01:28:23 -0300

    #182 - adicionado rota de criação de jogo

--- backend/api/routers/games_routes.py
+++ backend/api/routers/games_routes.py
@@ -1,5 +1,5 @@
 from api.database.config import Session, engine
-from api.schemas.games import GameSchema
+from api.schemas.games import GameSchema, GameInput
 from api.models.games import Game
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi.encoders import jsonable_encoder
@@ -13,18 +13,19 @@ router = APIRouter(
 
 session = Session(bind=engine)
 
+
 @router.get(
     "/",
     response_model=list[GameSchema],
     response_description="Sucesso de resposta da aplicação.",
 )
-
 async def getAll(skip: int = 0, limit: int = 100):
     query = session.query(Game)
     games = query.offset(skip).limit(limit).all()
 
     return jsonable_encoder(games)
-    
+
+
 @router.get(
     "/{id}",
     response_model=GameSchema,
@@ -36,3 +37,36 @@ async def getById(id: int):
         raise HTTPException(status_code=404, detail="Game not found")
     return jsonable_encoder(game)
 
+
+@router.post(
+    "/create",
+    status_code=201,
+    response_model=GameSchema,
+    response_description="Sucesso de resposta da aplicação.",
+)
+async def create(data: GameInput):
+    game = session.query(Game).filter(Game.id == data.id).first()
+    if game != None:
+        raise HTTPException(status_code=400, detail="Game already exists")
+    game_input = Game(name=data.name, id=data.id)
+    session.add(game_input)
+    session.commit()
+    session.refresh(game_input)
+    return jsonable_encoder(game_input)
+
+
+@router.delete(
+    "/delete/{id}",
+    status_code=200,
+    response_model=GameSchema,
+    response_description="Sucesso de resposta da aplicação.",
+)
+async def delete(id: int):
+    game = session.query(Game).filter(Game.id == id).first()
+    if game == None:
+        raise HTTPException(status_code=404, detail="Game not found")
+
+    session.delete(game)
+    session.commit()
+
+    return game
--- backend/api/schemas/games.py
+++ backend/api/schemas/games.py
@@ -1,11 +1,30 @@
 from typing import List, Optional
-from pydantic import BaseModel 
+from pydantic import BaseModel, Field, validator
 from datetime import datetime
- 
+
+
 class GameSchema(BaseModel):
     id: Optional[int] = None
     name: Optional[str] = None
     created_at: Optional[datetime] = None
-    
+
+    class Config:
+        orm_mode = True
+
+
+class GameInput(GameSchema):
+    id: int
+    name: str
+
+    @validator("name")
+    def check_empty_fields(cls, v):
+        assert v != "", "Empty strings are not allowed."
+        return v
+
+    @validator("name")
+    def check_spaced_fields(cls, v):
+        assert v.strip(), "Empty strings are not allowed."
+        return v
+
     class Config:
         orm_mode = True
